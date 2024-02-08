05552fa902eb75ac824700b3e4c72fdab15ad58e
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2024-02-08 00:07:34 -0300

    feat(footer): styled footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,30 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: center;
+  padding: 2rem 0;
+
+  > p {
+    ${(props) => props.theme.FontText.xSmall}
+    color: ${(props) => props.theme.color.text};
+
+    a {
+      text-decoration: underline;
+      color: ${(props) => props.theme.color.yellow};
+      &:hover {
+        color: ${(props) => props.theme.color["yellow-dark"]};
+      }
+    }
+  }
+`;
+
 export function Footer() {
-  return <p>footer</p>;
+  return (
+    <FooterContainer>
+      <p>
+        Developed by <a href="https://jakeliny.com"> Jakeliny Gracielly</a>
+      </p>
+    </FooterContainer>
+  );
 }
