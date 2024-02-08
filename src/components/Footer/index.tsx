8a037be110e4762a205db9e067bd3e8ff48148ec
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2024-02-08 00:11:15 -0300

    feat(footer): separete styled from footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,4 @@
-import styled from "styled-components";
-
-export const FooterContainer = styled.footer`
-  display: flex;
-  justify-content: center;
-  padding: 2rem 0;
-
-  > p {
-    ${(props) => props.theme.FontText.xSmall}
-    color: ${(props) => props.theme.color.text};
-
-    a {
-      text-decoration: underline;
-      color: ${(props) => props.theme.color.yellow};
-      &:hover {
-        color: ${(props) => props.theme.color["yellow-dark"]};
-      }
-    }
-  }
-`;
+import { FooterContainer } from "./styles";
 
 export function Footer() {
   return (
